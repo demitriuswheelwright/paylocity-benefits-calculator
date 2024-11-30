5a1cd97443edea18a19df3d7937727a0511bc52b
demitriuswheelwright <demitrius@att.net>
2024-11-30 15:20:52 -0600

    Updated endpoint to use IActionResult instead of ActionResult. Added [Produces] attribute

--- PaylocityBenefitsCalculator/Api/Controllers/EmployeesController.cs
+++ PaylocityBenefitsCalculator/Api/Controllers/EmployeesController.cs
@@ -17,9 +17,16 @@ public class EmployeesController : ControllerBase
         throw new NotImplementedException();
     }
 
+    /// <summary>
+    /// I try to return interfaces or base classes as often as possible.
+    /// I'm assuming the reason we're using the concrete class of ActionResult was to make
+    /// it clear what the endpoint returns, which we can accomplish by the [Produces] attribute.
+    /// </summary>
+    /// <returns></returns>
     [SwaggerOperation(Summary = "Get all employees")]
     [HttpGet("")]
-    public async Task<ActionResult<ApiResponse<List<GetEmployeeDto>>>> GetAll()
+    [Produces(typeof(ApiResponse<List<GetEmployeeDto>>))]
+    public async Task<IActionResult> GetAll()
     {
         //task: use a more realistic production approach
         
